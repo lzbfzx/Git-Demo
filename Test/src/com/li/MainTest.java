aaf2e71f4c169d1be3a03c5aff5e148f7890fdc2
lzbfzx <1879992541@qq.com>
2020-07-03 16:23:35 +0800

    合并分支

--- Test/src/com/li/MainTest.java
+++ Test/src/com/li/MainTest.java
@@ -3,6 +3,6 @@ package com.li;
 public class MainTest {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("test change");
+        System.out.println("hello master");
     }
 }
